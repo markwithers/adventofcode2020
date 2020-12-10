8447eb5d26660e6db8f86f9f9843db249e7cd3db
Mark Withers <mark.withers@candide.eu>
2020-12-10 13:53:22 +0000

    Day 8, part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -2,6 +2,8 @@ module Day8 where
 
 data Instruction = Nop Int | Acc Int | Jmp Int deriving (Show)
 
+data Result = Cycles Int | Terminates Int deriving (Show)
+
 parseLine :: [String] -> Instruction
 parseLine ("nop" : ('+' : n) : _) = Nop (read n)
 parseLine ("acc" : ('+' : n) : _) = Acc (read n)
@@ -10,17 +12,40 @@ parseLine ("nop" : ('-' : n) : _) = Nop (negate $ read n)
 parseLine ("acc" : ('-' : n) : _) = Acc (negate $ read n)
 parseLine ("jmp" : ('-' : n) : _) = Jmp (negate $ read n)
 
-performInstruction :: Int -> [Int] -> [Instruction] -> Int -> Int
-performInstruction acc seen is index
-  | index `elem` seen = acc
+replaceFn :: Int -> (a -> a) -> [a] -> [a]
+replaceFn i fn is = take i is ++ [fn (is !! i)] ++ drop (i + 1) is
+
+performInstruction :: Int -> [Int] -> Int -> [Instruction] -> Result
+performInstruction acc seen index is
+  | index >= length is = Terminates acc
+  | index `elem` seen = Cycles acc
   | otherwise = case i of
-    (Nop _) -> performInstruction acc (seen ++ [index]) is (index + 1)
-    (Acc n) -> performInstruction (acc + n) (seen ++ [index]) is (index + 1)
-    (Jmp n) -> performInstruction acc (seen ++ [index]) is (index + n)
+    (Nop _) -> performInstruction acc (seen ++ [index]) (index + 1) is
+    (Acc n) -> performInstruction (acc + n) (seen ++ [index]) (index + 1) is
+    (Jmp n) -> performInstruction acc (seen ++ [index]) (index + n) is
   where
     i = is !! index
+
+swap :: Instruction -> Instruction
+swap i = case i of
+  (Nop n) -> Jmp n
+  (Jmp n) -> Nop n
+  (Acc n) -> Acc n
+
+modifyInstructions :: [Instruction] -> [[Instruction]]
+modifyInstructions is = map (\i -> replaceFn i swap is) [0 .. length is - 1]
+
+terminates :: Result -> Bool
+terminates (Terminates _) = True
+terminates (Cycles _) = False
 
 day8 :: IO ()
 day8 = do
   x <- readFile "./inputs/day8"
-  print . (\is -> performInstruction 0 [] is 0) . map (parseLine . words) $ lines x
+  print . performInstruction 0 [] 0 . map (parseLine . words) $ lines x
+  print
+    . filter terminates
+    . map (performInstruction 0 [] 0)
+    . modifyInstructions
+    . map (parseLine . words)
+    $ lines x
